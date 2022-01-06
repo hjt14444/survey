57fa5611d53c0ed2f6cda8937c1199f0ed7e1922
hjt14444 <925186882@qq.com>
2022-01-06 21:25:05 +0800

    增加头部搜索框

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
+      <h1>我是feature-header-search-hjt</h1>
       <router-link to="/">home</router-link> |
       <router-link to="/survey">Survey</router-link>
     </div>
